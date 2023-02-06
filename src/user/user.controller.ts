92517c5505e493e441ea9a3532001735b2978ebe
Sayed-Nur-E-Newaz <miznewaz@gmail.com>
2023-02-06 23:16:37 +0600

    User Controller added

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+    constructor(private readonly userService: UserService){}
+
+    @Post("/create-user")
+    createnewUser(){
+        try {
+            return this.userService.createUser()
+        } catch (error) {
+            throw error
+        }
+    }
+
+
+}
+
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    constructor(private readonly prismaService: PrismaService){
+
+    }
+
+    async createUser(){
+        return this.prismaService.user.create({
+            data: {
+                name : "Jim Newaz",
+                email : "test@gmail.com"
+            }
+        })
+    }
+}
